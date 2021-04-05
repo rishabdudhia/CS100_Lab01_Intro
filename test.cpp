082f46ced020e3cd9fc97bc19765b43a85a35833
rishabdudhia <rishabdudhia@gmail.com>
2021-04-05 14:22:00 -0700

    Finished and passed all five test cases

--- test.cpp
+++ test.cpp
@@ -7,6 +7,29 @@ TEST(EchoTest, HelloWorld) {
     EXPECT_EQ("hello world", echo(3,test_val));
 }
 
+TEST(EchoTest, EmptyString) {
+    char* test_val[1]; test_val[0] = "./c-echo";
+    EXPECT_EQ("", echo(1,test_val));
+}
+
+TEST(EchoTest, Space)
+{
+	char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = " ";
+	EXPECT_EQ(" ", echo(2, test_val));
+}
+
+TEST(EchoTest, SimpleMath)
+{
+	char* test_val[6]; test_val[0] = "./c-echo"; test_val[1] = "12"; test_val[2] = "-"; test_val[3] = "4"; test_val[4] = "="; test_val[5] = "8";
+	EXPECT_EQ("12 - 4 = 8", echo(6, test_val));
+}
+
+TEST(EchoTest, Symbols)
+{
+	char* test_val[4]; test_val[0] = "./c-echo"; test_val[1] = "&"; test_val[2] = "!"; test_val[3] = "%";
+	EXPECT_EQ("& ! %", echo(4, test_val));
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
